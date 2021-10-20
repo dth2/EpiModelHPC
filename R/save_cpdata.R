41cb60b114827377f6935c8fcbe8b114720e624a
dth2 <dth2@users.noreply.github.com>
2021-10-20 13:04:43 -0700

    Update save_cpdata.R

--- R/save_cpdata.R
+++ R/save_cpdata.R
@@ -19,8 +19,7 @@ save_cpdata <- function(dat, at) {
   if (!is.null(dat$control$save.int) && at %% dat$control$save.int == 0) {
     currsim <- dat$control$currsim
     simno <- dat$control$simno
-    #fn <- paste0("data/sim", simno, "/sim", currsim, ".cp.rda")
-    fn <- paste0("data/sim", "TESTTEST",  ".cp.rda")
+    fn <- paste0("data/sim", simno, "/sim", currsim, ".cp.rda")
     dat$last.ts <- at
     x <- dat
     save(x, file = fn)
